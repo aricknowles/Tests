6d5745c859a1be45925f7b7d69e6a9a4a7f79486
aricknowles <32400511+aricknowles@users.noreply.github.com>
2018-12-19 14:15:15 -0500

    10

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).on('click','.scroll-link', function(event) {
+    var anchor = $(this).attr('href');
+    $('body, html').animate({
+      scrollTop: ($(anchor).offset().top)
+    }, 800);
+    return false
+});
