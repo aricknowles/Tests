8816a92ebe78e1476c17a93204e37119efd7f8b7
aricknowles <32400511+aricknowles@users.noreply.github.com>
2018-12-19 13:45:38 -0500

    update3

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
-$(function () {
-  $.scrollify({
-      section : ".panel",
-    interstitialSection : ".footer",
-      easing: "easeOutExpo",
-      scrollSpeed: 800,
-      offset : 0,
-      scrollbars: true,
-      standardScrollElements: "",
-      before:function() {},
-      after:function() {},
-      afterResize:function() {},
-      afterRender:function() {}
-    });
-});
 $(document).on('click','.scroll-link', function(event) {
     var anchor = $(this).attr('href');
     $('body, html').animate({
